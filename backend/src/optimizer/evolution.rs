8a6ed2a16aa994b3ab3ba3a486936b69c295f5fe
fortishield <161459699+FortiShield@users.noreply.github.com>
2026-04-15 10:36:00 +0000

    ✨ Update crossover and mutation logic

--- backend/src/optimizer/evolution.rs
+++ backend/src/optimizer/evolution.rs
@@ -2,22 +2,26 @@ use crate::optimizer::genome::Genome;
 
 pub fn crossover(a: &Genome, b: &Genome) -> Genome {
     Genome {
-        rsi_period: if fastrand::bool() { a.rsi_period } else { b.rsi_period },
+        rsi_period: if fastrand::bool() {
+            a.rsi_period
+        } else {
+            b.rsi_period
+        },
         buy_threshold: (a.buy_threshold + b.buy_threshold) / 2.0,
         sell_threshold: (a.sell_threshold + b.sell_threshold) / 2.0,
     }
 }
 
 pub fn mutate(genome: &mut Genome) {
-    if fastrand::bool(0.3) {
+    if fastrand::bool() {
         genome.buy_threshold += fastrand::f64() * 10.0 - 5.0;
     }
 
-    if fastrand::bool(0.3) {
+    if fastrand::bool() {
         genome.sell_threshold += fastrand::f64() * 10.0 - 5.0;
     }
 
-    if fastrand::bool(0.2) {
+    if fastrand::bool() {
         genome.rsi_period = fastrand::usize(5..30);
     }
 
